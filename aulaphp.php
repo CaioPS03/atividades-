6015b3682c6b9af9793f706c1e9b0f6a5e15e6c8
CaioPS03 <caio.silva8@alunos.unis.edu.br>
2022-03-09 20:47:52 -0300

    variável e função

--- aulaphp.php
+++ aulaphp.php
@@ -10,7 +10,28 @@ $nome = 'Maria';
 echo $nome;
 */
 
-$nome = 2;
-$sobrenome = 2;
+/*
+$nome = 'Caio';
+$sobrenome = 'Silva';
 
-echo $nome + $sobrenome;
+echo $nome . ' ' . $sobrenome;
+echo '<br>';
+
+echo "$nome $sobrenome";
+*/
+function soma($numero1, $numero2) {
+    return $numero1+$numero2;
+}
+
+
+$numero01 = 2;
+$numero02 = 2;
+$soma = soma($numero01, $numero02);
+echo $soma . '<br>';
+
+$numero01 = 3;
+$numero02 = 6;
+$soma = soma($numero01, $numero02);
+echo $soma . '<br>'; 
+
+echo soma(100, 20);
