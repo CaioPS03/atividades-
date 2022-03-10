0d1041b1d0362e8246a027ad83b7de18dd2f2c3c
CaioPS03 <caio.silva8@alunos.unis.edu.br>
2022-03-09 21:41:58 -0300

    classes, objetos e atributos

--- aulaphp.php
+++ aulaphp.php
@@ -3,14 +3,14 @@
 //variáveis em php
 //começam com $
 
-/*
+/*variáveis
 $nome = 'José <br>';
 echo $nome;
 $nome = 'Maria';
 echo $nome;
 */
 
-/*
+/*mais variáveis
 $nome = 'Caio';
 $sobrenome = 'Silva';
 
@@ -19,9 +19,16 @@ echo '<br>';
 
 echo "$nome $sobrenome";
 */
+
+/* funções
 function soma($numero1, $numero2) {
     return $numero1+$numero2;
 }
+
+function nomedela() {
+    return 'é jessica <br>';
+}
+echo nomedela();
 
 
 $numero01 = 2;
@@ -35,3 +42,58 @@ $soma = soma($numero01, $numero02);
 echo $soma . '<br>'; 
 
 echo soma(100, 20);
+*/
+
+/*classes
+classe alumo
+
+atributos
+- ra
+- nome
+- idade
+
+metodos
+- faz prova
+- assiste aula
+- faz matrícula
+- mata aula
+
+*/
+
+/*
+class Aluno
+{
+    //atributos
+    public $ra;
+    public $nome;
+    public $idade;
+    
+    //métodos
+    public function fazProva() {
+        return 'fiz prova';
+    }
+    
+    public function assiteAula() {
+        return 'assisti aula';
+    }
+    
+    public function fazMatrícula() {
+        return 'fiz matrícula';
+    }
+    
+    public function mataAula() {
+        return 'matei aula';
+    }
+}
+
+$aluno01 = new Aluno;
+$aluno01->ra = "2022100063";
+$aluno01->nome = "Caio PS";
+$aluno01->idade = 17;
+
+echo $aluno01->nome;
+echo $aluno01->mataAula();
+
+$aluno02 = new Aluno;
+$aluno->ra = "2022100056"
+*/
